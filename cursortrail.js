edcaecca98c3c4e9a98723ce3762cda935ae1cfc
DoogeTube <122920143+DoogeTube@users.noreply.github.com>
2024-04-06 14:51:03 -0400

    better mobile compatibility

--- cursortrail.js
+++ cursortrail.js
@@ -1,77 +1,80 @@
-function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext('2d');
+
+const pointer = {
+    x: .5 * window.innerWidth,
+    y: .5 * window.innerHeight,
+};
+const params = {
+    pointsNumber: 31,
+    widthFactor: 1,
+};
+
+const points = [];
+const rate = 1000;
+
+window.addEventListener("click", e => {
+    updateMousePosition(e.pageX, e.pageY);
+});
+window.addEventListener("mousemove", e => {
+    updateMousePosition(e.pageX, e.pageY);
+});
+window.addEventListener("touchstart", e => {
+    const touch = e.touches[0];
+    updateMousePosition(touch.pageX, touch.pageY);
+});
+window.addEventListener("touchmove", e => {
+    const touch = e.touches[0];
+    updateMousePosition(touch.pageX, touch.pageY);
+});
+
+function updateMousePosition(eX, eY) {
+    pointer.x = (eX - 6);
+    pointer.y = (eY + 4);
 }
 
-if (!isMobile()) {
-    const canvas = document.querySelector("canvas");
-    const ctx = canvas.getContext('2d');
 
-    const pointer = {
-        x: .5 * window.innerWidth,
-        y: .5 * window.innerHeight,
-    };
-    const params = {
-        pointsNumber: 31,
-        widthFactor: 1,
-    };
 
-    const points = [];
-    const rate = 1000;
-
-    window.addEventListener("click", e => {
-        updateMousePosition(e.pageX, e.pageY);
-    });
-    window.addEventListener("mousemove", e => {
-        updateMousePosition(e.pageX, e.pageY);
-    });
-
-    function updateMousePosition(eX, eY) {
-        pointer.x = (eX - 6);
-        pointer.y = (eY + 4);
+setupCanvas();
+setInterval(drawLines, 16.666);
+function addPoint() {
+    let x = pointer.x
+    let y = pointer.y
+    points.push({ x: x, y: y });
+    if (points.length > params.pointsNumber) {
+        points.shift();
     }
+}
 
+window.addEventListener("resize", setupCanvas);;
 
+function drawLines() {
+    addPoint()
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    setupCanvas();
-    setInterval(drawLines, 16.666);
-    function addPoint() {
-        let x = pointer.x
-        let y = pointer.y
-        points.push({ x: x, y: y });
-        if (points.length > params.pointsNumber) {
-            points.shift();
-        }
-    }
+    ctx.globalAlpha = 0.1;
+    ctx.strokeStyle = "yellow";
 
-    window.addEventListener("resize", setupCanvas);;
+    ctx.lineCap = "round";
+    ctx.beginPath();
 
-    function drawLines() {
-        addPoint()
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.moveTo(points[0].x, points[0].y);
 
-        ctx.globalAlpha = 0.1;
-        ctx.strokeStyle = "yellow";
-
-        ctx.lineCap = "round";
-        ctx.beginPath();
-
-        ctx.moveTo(points[0].x, points[0].y);
-
-        for (let i = 1; i < points.length - 1; i++) {
-            const xc = .5 * (points[i].x + points[i + 1].x);
-            const yc = .5 * (points[i].y + points[i + 1].y);
-            ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
-            ctx.lineWidth = params.widthFactor * (i);
-            ctx.stroke();
-        }
-
-        ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
+    for (let i = 1; i < points.length - 1; i++) {
+        const xc = .5 * (points[i].x + points[i + 1].x);
+        const yc = .5 * (points[i].y + points[i + 1].y);
+        ctx.quadraticCurveTo(points[i].x, points[i].y, xc, yc);
+        ctx.lineWidth = params.widthFactor * (i);
         ctx.stroke();
     }
 
+    ctx.lineTo(points[points.length - 1].x, points[points.length - 1].y);
+    ctx.stroke();
+}
 
-    function setupCanvas() {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    }
+
+function setupCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
